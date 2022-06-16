601ddac6605d604bda483c35c52fa4a4463d4508
Michael Hayes <Michael.Hayes@Mediclinic.co.za>
2022-06-16 20:09:19 +0200

    project setup

---
+++ src/pages/home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "@mui/material";
+type Props = {};
+
+const Index = (props: Props) => {
+  return (
+    <div>
+      <Button variant="outlined">test</Button>
+    </div>
+  );
+};
+
+export default Index;
